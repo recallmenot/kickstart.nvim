ea603f1176dd242362adc903563e1ec16b1065e2
Edmund Raile <edmund.raile@proton.me>
2025-08-15 13:11:03 +0200

    codecompanion conditional enable on private existence

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,36 +1,46 @@
-return {
-  'olimorris/codecompanion.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-treesitter/nvim-treesitter',
-  },
-  config = function()
-    local private = require 'custom.private'
-    require('codecompanion').setup {
-      adapters = {
-        ollama = function()
-          return require('codecompanion.adapters').extend('openai_compatible', {
-            env = {
-              url = private.get_key 'llm_url', -- optional: default value is ollama url http://127.0.0.1:11434
-              api_key = private.get_key 'llm_api_key', -- optional: if your endpoint is authenticated
-              chat_url = private.get_key 'llm_completions', -- optional: default value, override if different
-            },
-          })
-        end,
-      },
-      strategies = {
-        chat = {
-          adapter = 'ollama',
+-- plugins/codecompanion.lua
+local private_module_exists, private = pcall(require, 'custom.private')
+
+local codecompanion_config = {}
+
+if private_module_exists then
+  codecompanion_config = {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('codecompanion').setup {
+        adapters = {
+          ollama = function()
+            return require('codecompanion.adapters').extend('openai_compatible', {
+              env = {
+                url = private.get_key 'llm_url',
+                api_key = private.get_key 'llm_api_key',
+                chat_url = private.get_key 'llm_completions',
+              },
+            })
+          end,
         },
-        inline = {
-          adapter = 'ollama',
+        strategies = {
+          chat = {
+            adapter = 'ollama',
+          },
+          inline = {
+            adapter = 'ollama',
+          },
         },
-      },
-    }
-    vim.keymap.set({ 'n', 'o', 'x' }, '<leader>ccx', '<cmd>CodeCompanionChat Add<cr>', { desc = 'CodeCompanion add' })
-    vim.keymap.set({ 'n', 'o', 'x' }, '<leader>cct', '<cmd>CodeCompanionChat Toggle<cr>', { desc = 'CodeCompanion toggle' })
-    vim.keymap.set({ 'n', 'o', 'x' }, '<leader>cca', '<cmd>CodeCompanionActions<cr>', { desc = 'CodeCompanion actions' })
-  end,
-  lazy = false,
-}
--- use '#buffer' to access the current buffer
+      }
+      vim.keymap.set({ 'n', 'o', 'x' }, '<leader>ccx', '<cmd>CodeCompanionChat Add<cr>', { desc = 'CodeCompanion add' })
+      vim.keymap.set({ 'n', 'o', 'x' }, '<leader>cct', '<cmd>CodeCompanionChat Toggle<cr>', { desc = 'CodeCompanion toggle' })
+      vim.keymap.set({ 'n', 'o', 'x' }, '<leader>cca', '<cmd>CodeCompanionActions<cr>', { desc = 'CodeCompanion actions' })
+    end,
+    lazy = false,
+  }
+else
+  -- Return an empty plugin table if private.lua doesn't exist
+  codecompanion_config = {}
+end
+
+return codecompanion_config
