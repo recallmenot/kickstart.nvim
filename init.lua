06b806768feee7b744abd3407a764e08623d6feb
Edmund Raile <edmund.raile@proton.me>
2026-03-10 14:08:51 +0100

    enable other LSPs: pyright, gopls, rust-analyzer

--- init.lua
+++ init.lua
@@ -594,9 +594,9 @@ require('lazy').setup({
       --  See `:help lsp-config` for information about keys and how to configure
       local servers = {
         clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        gopls = {},
+        pyright = {},
+        rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
